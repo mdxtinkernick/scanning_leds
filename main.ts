9b495fd57bf405b51573f6a89800dd391e403aaf
mdxtinkernick <n.weldin@mdx.ac.uk>
2021-01-18 14:08:10 +0000

    added a third dot (not working yet as I didnt add additional variables)

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ time_between_changes = 200
 let time_to_change = 0
 let strip = neopixel.create(DigitalPin.P0, num_leds, NeoPixelMode.RGB)
 basic.forever(function () {
+    let time_between_changes2 = 0
     if (control.millis() >= time_to_change) {
         time_to_change = control.millis() + time_between_changes
         strip.setPixelColor(dot_pos, neopixel.colors(NeoPixelColors.Black))
@@ -23,7 +24,19 @@ basic.forever(function () {
         }
     }
     if (control.millis() >= time_to_change2) {
-        let time_between_changes2 = 0
+        time_to_change2 = control.millis() + time_between_changes2
+        strip.setPixelColor(dot_pos2, neopixel.colors(NeoPixelColors.Black))
+        dot_pos2 += movement2
+        strip.setPixelColor(dot_pos2, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        if (dot_pos2 == num_leds - 1) {
+            movement2 = -1
+        }
+        if (dot_pos2 == 0) {
+            movement2 = 1
+        }
+    }
+    if (control.millis() >= time_to_change2) {
         time_to_change2 = control.millis() + time_between_changes2
         strip.setPixelColor(dot_pos2, neopixel.colors(NeoPixelColors.Black))
         dot_pos2 += movement2
